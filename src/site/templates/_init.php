09dff89b50a4863b63a706d0be89a8d0f39b84b3
Léo Martin <github@leomartin.net>
2016-04-29 16:23:53 +0200

    + support du MarkDown

--- src/site/templates/_init.php
+++ src/site/templates/_init.php
@@ -1,3 +1,10 @@
 <?php
-  $title = $page->get('title');
+  /* objet data : contient les informations à passer aux vues */
+  $data = new stdClass;
+
+  /* Support du markdown */
+  $markdown_module_path = "../../wire/modules/Textformatter/TextformatterMarkdownExtra/";
+  require $markdown_module_path."parsedown/Parsedown.php";
+  require $markdown_module_path."parsedown-extra/ParsedownExtra.php";
+  $Extra = new ParsedownExtra();
 ?>
