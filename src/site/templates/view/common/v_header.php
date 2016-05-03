ccd5ca733ddcc2c22e3aaecedc9a9309aaf3ae32
Léo Martin <github@leomartin.net>
2016-05-03 15:29:57 +0200

    + variable $script -> permet de placer des scripts dans le header

--- src/site/templates/view/common/v_header.php
+++ src/site/templates/view/common/v_header.php
@@ -1,1 +1,2 @@
-<!DOCTYPE html><html lang="<?php echo $lang ?>"><head><meta charset="utf-8"><title><?php echo $page_title ?></title><link rel="stylesheet" href="<?php echo $config->urls->templates?>styles/s.css"></head><body>
+<!DOCTYPE html><html lang="<?php echo $lang ?>"><head><meta charset="utf-8"><title><?php echo $page_title ?></title><link rel="stylesheet" href="<?php echo $config->urls->templates?>styles/s.css">
+<?php if ($scripts) { echo $scripts; } ?></head><body>
