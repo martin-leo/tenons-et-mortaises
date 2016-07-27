a088f8ac7c7f7e4dcb011b0e6b55d9ba01b0568d
Léo Martin <bonjour@leomartin.net>
2016-07-27 17:18:09 +0200

    ajoute détection tactile v.0

---
+++ src/site/scripts/00_device.js
@@ -0,0 +1,20 @@
+var device = (function(){
+  /* Regroupe des informations sur le device qui browse le site ! alerte jargon
+  Void -> Void */
+  var device = {};
+  device.touch;
+
+  function constructor(){
+    /* constructeur
+    Void -> Void */
+    try {
+      device.touch = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0) || (typeof el.ongesturestart == "function");
+    } catch (e) {
+      device.touch = false;
+    }
+  }
+
+  constructor();
+  return device;
+})();
+console.log(device);
