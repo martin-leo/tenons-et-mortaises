3b006a53425798c0e56a9e032cf20b91530387fd
Léo Martin <bonjour@leomartin.net>
2016-07-29 18:12:12 +0200

    début de mise en place de la structure TeM

--- src/site/templates/view/common/v_header.php
+++ src/site/templates/view/common/v_header.php
@@ -1,2 +1,24 @@
 <!DOCTYPE html><html lang="<?php echo $lang ?>"><head><meta charset="utf-8"><title><?php echo $page_title ?></title><link rel="stylesheet" href="<?php echo $config->urls->templates?>styles/s.css">
 <?php if ($scripts) { echo $scripts; } ?></head><body>
+
+/* titre principal */
+<h1 hidden>titre du site - titre du contenu</h1>
+
+/* cartographie des archives */
+<section id="carte">
+  <h2 hidden>Cartographie des archives</h2>
+</section>
+
+/* Navigation */
+<nav>
+  <h2>Menu</h2>
+  <ul>
+    <li>événements</li>
+    <li>thèmes</li>
+    <li>objets récents</li>
+    <li>projets</li>
+    <li>archives</li>
+    <li>manifeste</li>
+    <li>contact</li>
+  </ul>
+</nav>
--- src/site/templates/view/pages/v_home.php
+++ src/site/templates/view/pages/v_home.php
@@ -1,1 +1,6 @@
+/* Contenu */
+<main>
+  <h2>titre de l'objet</h2>
+</main>
 
+/* Footer ? */
