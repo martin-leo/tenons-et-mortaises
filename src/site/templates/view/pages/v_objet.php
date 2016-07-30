aa22bb27bd4bc6f8a591d69f347e52218e12dee6
Léo Martin <bonjour@leomartin.net>
2016-07-30 19:02:07 +0200

    révision structure et styles page objet

--- src/site/templates/view/pages/v_objet.php
+++ src/site/templates/view/pages/v_objet.php
@@ -3,12 +3,14 @@
     * 1. article
       * 1.1. div : conteneur non-sémantique
         * 1.1.1. header : titre, auteurs, etc.
-        * 1.1.2. contenu de l'objet
-      * 1.2. aside : objets associés, thème, etc.
-        * 1.2.1. titre
-        * 1.2.2. thèmes
-        * 1.2.3. objets associés
-        * 1.2.4. satellites
+      * 1.2. div : conteneur non-sémantique
+        * 1.2.1 div : conteneur non-sémantique
+          * 1.2.1.2. contenu de l'objet
+        * 1.2.2. aside : objets associés, thème, etc.
+          * 1.2.2.1. titre
+          * 1.2.2.2. thèmes
+          * 1.2.2.3. objets associés
+          * 1.2.2.4. satellites
   */
   /*
       1. article : le contenu de l'objet à proprement parler
@@ -27,32 +29,42 @@
         <?php $format->date_tem($page); ?>
         <?php $format->imprimer_dates_et_origine($page); ?>
       </header>
-      /*
-          1.1.2. contenu de l'objet
-      */
-      <section>
-        <?php echo $Extra->text($page->contenu) ?>
-      </section>
     </div>
     /*
-        1.2. aside : objets associés, thème, etc.
+        1.2. conteneur non-sémantique
     */
-    <aside>
-      /* titrage */
-      <h2>Éléments associés</h2>
+    <div>
       /*
-          1. thèmes
+          1.2.1. contenu de l'objet
       */
-      <?php $format->imprimer_themes($page); ?>
+      <div>
+        /*
+            1.1.2. contenu de l'objet
+        */
+        <section>
+          <?php echo $Extra->text($page->contenu) ?>
+        </section>
+      </div>
       /*
-          2. objets associés
-      */
-      <?php $format->imprimer_objets_associes($page); ?>
-      /*
-          3. satellites
-      */
-      <?php $format->imprimer_satellites($page); ?>
-    </aside>
+          1.2.2 aside : objets associés, thème, etc.
+          */
+      <aside>
+        /* titrage */
+        <h2>Éléments associés</h2>
+        /*
+            1. thèmes
+        */
+        <?php $format->imprimer_themes($page); ?>
+        /*
+            2. objets associés
+        */
+        <?php $format->imprimer_objets_associes($page); ?>
+        /*
+            3. satellites
+        */
+        <?php $format->imprimer_satellites($page); ?>
+      </aside>
+    </div>
   </article>
   /* aside : objets associés, thèmes, projet */
 </main>
