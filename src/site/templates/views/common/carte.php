722aeb1f1f4e5518270bfe86a0f0df96875fa416
Léo Martin <bonjour@leomartin.net>
2017-08-09 18:09:10 +0200

    carte active à l'ouverture si page accueil

--- src/site/templates/views/common/carte.php
+++ src/site/templates/views/common/carte.php
@@ -1,6 +1,6 @@
 /* <!-- aside : carte --> */
 
-<aside id="carte" class="carte active">
+<aside id="carte" class="carte<?php if ( $page->id == 1) { echo " active"; } ?>">
 	<h2 class="carte__titre">carte</h2>
 
 	/* <!-- carte au format SVG --> */
