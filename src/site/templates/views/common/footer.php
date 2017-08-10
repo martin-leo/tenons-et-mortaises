79b6398296959d809c29cc319f425a1cae297059
Léo Martin <bonjour@leomartin.net>
2017-08-10 16:20:57 +0200

    php : gestion scripts additionnels

--- src/site/templates/views/common/footer.php
+++ src/site/templates/views/common/footer.php
@@ -1,1 +1,1 @@
-<script src="<?php echo $config->urls->templates?>scripts/s.js"></script></body></html>
+<script src="<?php echo $config->urls->templates?>scripts/s.js"></script><?php if ( isset ( $scripts ) ) { echo $scripts; } ?></body></html>
--- src/site/templates/views/common/header.php
+++ src/site/templates/views/common/header.php
@@ -5,5 +5,6 @@
     <title><?php echo $page->title ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo $config->urls->templates?>styles/s.css">
+    <?php if ( isset ( $head__scripts ) ) { echo $head__scripts; } ?>
   </head>
   <body class="page">
